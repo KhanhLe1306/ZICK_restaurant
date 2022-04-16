14b109393bfb26dad47db576ab874d90658f02c0
lqk1306 <103775167+lqk1306@users.noreply.github.com>
2022-04-15 22:07:13 -0500

    Update Login.java

--- zick_restaurant/src/main/java/controllers/Login.java
+++ zick_restaurant/src/main/java/controllers/Login.java
@@ -1,11 +1,16 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import util.DBUtil;
 
 /**
  * Servlet implementation class Login
@@ -13,29 +18,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Login() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		System.out.println("inside the post request Login servlet");
+		
+		int id = DBUtil.getIdentity(request.getParameter("username"), request.getParameter("password"));
+		//id = 0 => person does not exit
+		//otherwise, person is either customer or admin
+		System.out.println(id);
+		
+		if(id == 0) {
+			request.setAttribute("message", "Customer does not exit!");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+			dispatcher.forward(request, response);
+		}else {
+			int adminId = DBUtil.getAdminId(id);
+			if(adminId == 0) { //not an admin
+				HttpSession session = request.getSession();
+				session.setAttribute("customerId", id);
+				response.sendRedirect("home.jsp");
+			}else {
+				HttpSession session = request.getSession();
+				session.setAttribute("adminId", id);
+				response.sendRedirect("home.jsp");
+			}
+		}
 	}
 
 }
