c345d13e91bccc9b5ccf8bb19870bfa4b46b3cbd
khanh <khanhle@unomaha.edu>
2022-04-16 12:37:55 -0500

    dont display 0 quantity item in cart

--- zick_restaurant/src/main/java/controllers/Cart.java
+++ zick_restaurant/src/main/java/controllers/Cart.java
@@ -45,13 +45,14 @@ public class Cart extends HttpServlet {
 			Enumeration paramNames = request.getParameterNames();	// get all "name" parameters
 			while(paramNames.hasMoreElements()) {
 				String productCode = (String)paramNames.nextElement(); //1st 4th 7th ... will be product code
-				System.out.println("Product code: " + productCode);
-				System.out.println("Product name: " +request.getParameter(productCode + "_name"));
-				System.out.println("Price: " +request.getParameter(productCode+ "_price"));
-				System.out.println("Quantity: "+request.getParameter(productCode + "_quantity")); // use the product code with _quantity to get the quantity.
-				System.out.println("Product description: " +request.getParameter(productCode + "_description"));
-				cart.add(new CartProduct(productCode,request.getParameter(productCode + "_name"), request.getParameter(productCode+ "_price"),request.getParameter(productCode + "_description"), request.getParameter(productCode + "_quantity")  ));
-				//session.setAttribute("cart", cart);
+				if (!request.getParameter(productCode + "_quantity").equalsIgnoreCase("0")) {
+					System.out.println("Product code: " + productCode);
+					System.out.println("Product name: " +request.getParameter(productCode + "_name"));
+					System.out.println("Price: " +request.getParameter(productCode+ "_price"));
+					System.out.println("Quantity: "+request.getParameter(productCode + "_quantity")); // use the product code with _quantity to get the quantity.
+					System.out.println("Product description: " +request.getParameter(productCode + "_description"));
+					cart.add(new CartProduct(productCode,request.getParameter(productCode + "_name"), request.getParameter(productCode+ "_price"),request.getParameter(productCode + "_description"), request.getParameter(productCode + "_quantity")  ));
+				}
 				paramNames.nextElement();
 				paramNames.nextElement();
 				paramNames.nextElement();
