48c6851d169ce3e5b4fa888c8e0a3e40200dbad9
lqk1306 <103775167+lqk1306@users.noreply.github.com>
2022-04-15 22:12:59 -0500

    Update Signup.java

--- zick_restaurant/src/main/java/controllers/Signup.java
+++ zick_restaurant/src/main/java/controllers/Signup.java
@@ -1,11 +1,15 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.DBUtil;
 
 /**
  * Servlet implementation class Signup
@@ -14,28 +18,36 @@ import javax.servlet.http.HttpServletResponse;
 public class Signup extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Signup() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		System.out.println("inside the post request Signup servlet");
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		boolean isValidCustomer = DBUtil.signup(firstName, lastName, userName, password);
+		
+		if (isValidCustomer) {
+			request.setAttribute("message", "Successfully registration! Please log in and order you food now!");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+			dispatcher.forward(request, response);
+		}else {
+			request.setAttribute("message", "Username exits! Please try other username");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("signup.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
